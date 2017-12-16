fc14b208a98532b835c2f550e77fc39403fcb23c
Brendan Kowitz <brendan.kowitz@gmail.com>
2017-12-15 18:48:38 -0800

    Update Startup.sh

--- Startup.sh
+++ Startup.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# Precompile app
-cd /home/app/crucible
-RAILS_ENV=production rake assets:precompile
-
-# Start background job
+su root -c 'mongod --fork --syslog'
 su root -c 'export PATH=$PATH:/usr/local/bin/ruby ; RAILS_ENV=production /home/app/crucible/bin/delayed_job -n3 start'
 
-# Start nginx
 /sbin/my_init
